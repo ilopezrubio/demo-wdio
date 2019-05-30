c1b7b7f216ca47579e562243d18b8cc8c24d513a
LuisDa9529 <daniel-case@hotmail.com>
2019-05-30 16:33:47 -0500

    Added .features and steps

---
+++ features/flights.feature
@@ -0,0 +1,6 @@
+Feature: Flights
+
+    Scenario: Book a flight
+    Given that a user is on the main page
+    When the user looks for a flight
+    Then the user selects a flight to book 
---
+++ features/steps/Flights.steps.js
@@ -0,0 +1,11 @@
+this.Given("that a user is on the main page", function () {
+  return "pending";
+});
+
+this.Then("the user looks for a flight", function () {
+  return "pending";
+});
+
+this.Then("the user selects a flight to book ", function () {
+  return "pending";
+});
