9c5a84bb482fcaf1e371f2d7136d2c0353ccc472
Josué Isaí Romero Barrera <jromerotf@outlook.com>
2019-05-30 12:09:11 -0500

    se agregan 5 selectors

--- features/page_objects/main.page.js
+++ features/page_objects/main.page.js
@@ -1,7 +1,7 @@
 export class Page {
     constructor() {
-
     }
+<<<<<<< HEAD
     get hotels() {return browser.element('span.hidden-xs:nth-child(4)');}
     get flights() {return browser.element('.nav-tabs > li:nth-child(2) > a:nth-child(1) > span:nth-child(3)');}
     get tours() {return browser.element('li.text-center:nth-child(3) > a:nth-child(1) > span:nth-child(3)');}
@@ -10,3 +10,13 @@ export class Page {
 
 }
 
+=======
+
+    get hotelillos () { return browser.element('//*[@id="body-section"]/section/div[2]/div/div/div[2]/ul/li[1]/a'); }
+    get vuelillos () { return browser.element('//*[@id="body-section"]/section/div[2]/div/div/div[2]/ul/li[2]'); }
+    get toursillos () { return browser.element('//*[@id="body-section"]/section/div[2]/div/div/div[2]/ul/li[3]'); }
+    get carsillos () { return browser.element('//*[@id="body-section"]/section/div[2]/div/div/div[2]/ul/li[4]'); }
+    get searchbtnillo () { return browser.element('//*[@id="flights"]/form/div[6]/button'); }
+
+}
+>>>>>>> se agregan 5 selectors
