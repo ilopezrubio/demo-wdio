4bb8567ea44403a54d09ed5a7d76629cbe7a79c8
Miguel Horacio Hernandez Balderas <miguel_horaacio@hotmail.com>
2019-05-30 16:26:47 -0500

    Este si jala, creeme prro

---
+++ features/carrs.feature
@@ -0,0 +1,9 @@
+Feature: Car
+
+    Scenario: Search a car to rent
+        Given we are in the main travels page
+        When the required information is set
+        Then we return the best car option to rent
+
+
+
---
+++ features/steps/cars.steps.js
@@ -0,0 +1,11 @@
+this.Given(/^we are in the main travels page$/, function() {
+  return "pending";
+});
+
+this.When(/^the required information is set$/, function() {
+  return "pending";
+});
+
+this.Then(/^we return the best car option to rent$/, function() {
+  return "pending";
+});
