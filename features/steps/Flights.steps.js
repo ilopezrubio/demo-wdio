d0df434dd427261f6f66ddd73d0170dd63723311
LuisDa9529 <daniel-case@hotmail.com>
2019-05-31 14:53:55 -0500

    Listo y mejorado

--- features/steps/Flights.steps.js
+++ features/steps/Flights.steps.js
@@ -1,11 +1,11 @@
-this.Given("that a user is on the main page", function () {
+Given("that a user is on the main page", function () {
   return "pending";
 });
 
-this.Then("the user looks for a flight", function () {
+When("the user looks for a flight", function () {
   return "pending";
 });
 
-this.Then("the user selects a flight to book ", function () {
+Then("the user selects a flight to book ", function () {
   return "pending";
 });
