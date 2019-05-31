ac7388dbe24aeedc4012616bf32344796aa47a4f
miguelhoracio <miguel_horaacio@hotmail.com>
2019-05-31 15:37:58 -0500

    Esta ya jala ahora si neta neta y esta mas chido

--- features/steps/cars.steps.js
+++ features/steps/cars.steps.js
@@ -1,11 +1,9 @@
-this.Given(/^we are in the main travels page$/, function() {
+Given(/^we are in the main travels page$/, function() {
   return "pending";
 });
-
-this.When(/^the required information is set$/, function() {
+When(/^the required information is set$/, function() {
   return "pending";
 });
-
-this.Then(/^we return the best car option to rent$/, function() {
+Then(/^we return the best car option to rent$/, function() {
   return "pending";
 });
